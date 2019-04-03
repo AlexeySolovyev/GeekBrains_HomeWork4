f75e2fcff730482b9362f9ed2cd5b5ae9bcfc32e
Alexey Solovyev <solovevalex@gmail.com>
2019-04-03 16:22:58 +0300

    Bugs in the Employee class has been fixed

--- src/Tasks_1_2_3/EmployeeTest.java
+++ src/Tasks_1_2_3/EmployeeTest.java
@@ -1,19 +1,12 @@
 package Tasks_1_2_3;
 
-public class EmployeeTest {
-    public static void main(String[] args) {
-        Employee employee1 = new Employee("Alex", "Bolton", "QA Engineer", "abolton@somecompany.com", 35000,29);
-        System.out.println(employee1);
-    }
-}
-
 class Employee {
-    private String firstName;
-    private String lastName;
-    private String position;
-    private String email;
-    private int salary;
-    private int age;
+    String firstName;
+    String lastName;
+    String position;
+    String email;
+    int salary;
+    int age;
 
     public Employee (String firstName, String lastName, String position, String email, int salary, int age) {
         this.firstName = firstName;
@@ -23,7 +16,16 @@ class Employee {
         this.salary = salary;
         this.age = age;
     }
+}
 
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Alex", "Bolton", "QA Engineer", "abolton@somecompany.com", 35000,29);
+        Employee employee2 = new Employee("Bob", "Marley", "Sofware Engineer", "bmarley@somecompany.com", 76000,34);
+
+        System.out.println(employee1.firstName + " " + employee1.lastName + ", " + employee1.position + ", email:" + employee1.email + ", salary: $" + employee1.salary + ", age: " + employee1.age);
+        System.out.println(employee2.firstName + " " + employee2.lastName + ", " + employee2.position + ", email:" + employee2.email + ", salary: $" + employee2.salary + ", age: " + employee2.age);
+    }
 }
 
 
